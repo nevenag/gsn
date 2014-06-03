7e363140f82369f03756e73444d2c6326fb77bd8
Nevena <nevena.golubovic@gmail.com>
2014-06-03 04:57:45 -0700

    double ...

--- src/gsn/wrappers/RemoteEventPushWrapper.java
+++ src/gsn/wrappers/RemoteEventPushWrapper.java
@@ -125,7 +125,7 @@ public class RemoteEventPushWrapper extends AbstractWrapper{
 				JSONObject field = fields.getJSONObject(i);
 				field_names[i] = field.get("name").toString();
 				field_bytes[i] = DataTypes.DOUBLE;
-				field_values[i] = Double.parseDouble(field.get("value").toString());				
+				field_values[i] = (Double)field.get("value");				
 			}
 			
 			return new StreamElement(field_names, field_bytes, field_values, System.currentTimeMillis());
